c952e0b28ba1f9f8d086cfd5b6c0b83f8f72af5e
Kevin Yank <thatguy@kevinyank.com>
2014-05-23 17:10:09 +1000

    Specify whether rate limit is per-server using :per_server option.

--- spec/sidekiq-rate-limiter/fetch_spec.rb
+++ spec/sidekiq-rate-limiter/fetch_spec.rb
@@ -13,6 +13,18 @@ describe Sidekiq::RateLimiter::Fetch do
                       }
       def perform(*args); end
     end
+
+    class PerServerJob
+      include Sidekiq::Worker
+      sidekiq_options 'queue'    => 'basic',
+                      'retry'    => false,
+                      'rate' => {
+                          'limit'  => 1,
+                          'per_server' => true,
+                          'period' => 1
+                      }
+      def perform(*args); end
+    end
   end
 
   let(:options)       { { queues: [queue, another_queue, another_queue] } }
@@ -20,6 +32,7 @@ describe Sidekiq::RateLimiter::Fetch do
   let(:another_queue) { 'some_other_queue' }
   let(:args)          { ['I am some args'] }
   let(:worker)        { Job }
+  let(:server_worker) { PerServerJob }
   let(:redis_class)   { Sidekiq.redis { |conn| conn.class } }
 
   it 'should inherit from Sidekiq::BasicFetch' do
@@ -61,21 +74,28 @@ describe Sidekiq::RateLimiter::Fetch do
     q.size.should == 1
   end
 
-  it 'should not rate-limit work on a different host', queuing: true do
+  it 'should rate-limit work across all hosts by default', queuing: true do
     fetch = described_class.new(options)
 
     worker.perform_async(*args)
     worker.perform_async(*args)
 
     fetch.retrieve_work
+    fetch.retrieve_work.should be_nil
+  end
+
+  it 'should rate-limit work per-host when specified in job options', queuing: true do
+    fetch = described_class.new(options)
+
+    server_worker.perform_async(*args)
+    server_worker.perform_async(*args)
+
+    fetch.retrieve_work
 
     Sidekiq::RateLimiter::Limit.any_instance.should_receive(:hostname).and_return('not_this_host')
 
     work = fetch.retrieve_work
-    JSON.parse(work.message)['class'].should eq 'Job'
-
-    q = Sidekiq::Queue.new(queue)
-    q.size.should == 0
+    JSON.parse(work.message)['class'].should eq server_worker.name
   end
 
 end
