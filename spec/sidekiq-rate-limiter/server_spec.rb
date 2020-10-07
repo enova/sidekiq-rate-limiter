391f00c5fef9d5d366b822ea66e34230474cf657
anero <anero79@gmail.com>
2020-10-07 15:49:16 -0300

    A refactoring in the internal Fetch API was introduced in Sidekiq 6.1.0 that is not backwards compatible.
    As part of this refactoring the object in the :fetch option is expected
    to be an instance of Sidekiq::BasicFetch or an object that responds to
    the same methods.
    
    This change allows using the gem with newer versions while keeping
    compatibility with old ones. See https://github.com/mperham/sidekiq/pull/4602 for more details.

--- spec/sidekiq-rate-limiter/server_spec.rb
+++ spec/sidekiq-rate-limiter/server_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Sidekiq::RateLimiter, 'server configuration' do
 
   it 'should set Sidekiq.options[:fetch] as desired' do
     Sidekiq.configure_server do |config|
-      expect(Sidekiq.options[:fetch]).to eql(Sidekiq::RateLimiter::Fetch)
+      expect(Sidekiq.options[:fetch]).to be_an_instance_of(Sidekiq::RateLimiter::Fetch)
     end
   end
 
   it 'should inherit from Sidekiq::BasicFetch' do
     Sidekiq.configure_server do |config|
-      expect(Sidekiq.options[:fetch]).to be < Sidekiq::BasicFetch
+      expect(Sidekiq.options[:fetch].class).to be < Sidekiq::BasicFetch
     end
   end
 end
