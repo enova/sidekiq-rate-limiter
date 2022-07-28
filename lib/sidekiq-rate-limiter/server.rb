150a5b8a3c361acd8d3b1df275f66689a4836600
anero <anero79@gmail.com>
2022-07-28 13:00:39 -0300

    Add support for new Sidekiq's configuration model introduced in https://github.com/mperham/sidekiq/pull/5340

--- lib/sidekiq-rate-limiter/server.rb
+++ lib/sidekiq-rate-limiter/server.rb
@@ -2,5 +2,12 @@ require 'sidekiq-rate-limiter/version'
 require 'sidekiq-rate-limiter/fetch'
 
 Sidekiq.configure_server do |config|
-  Sidekiq.options[:fetch] = Sidekiq::RateLimiter::Fetch
+  # Backwards compatibility for Sidekiq < 6.1.0 (see https://github.com/mperham/sidekiq/pull/4602 for details)
+  if (Sidekiq::BasicFetch.respond_to?(:bulk_requeue))
+    Sidekiq.options[:fetch] = Sidekiq::RateLimiter::Fetch
+  elsif (Sidekiq::VERSION < '6.5.0') # Sidekiq config was redesigned in https://github.com/mperham/sidekiq/pull/5340
+    Sidekiq.options[:fetch] = Sidekiq::RateLimiter::Fetch.new(Sidekiq.options)
+  else
+    Sidekiq[:fetch] = Sidekiq::RateLimiter::Fetch.new(Sidekiq)
+  end
 end
