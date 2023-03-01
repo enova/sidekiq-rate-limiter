45cd596de24a629bf64c27144cf41f13d71bbe4d
Robert Nubel <rnubel@enova.com>
2023-03-01 10:13:01 -0600

    Update Coveralls to use GitHub action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,24 @@ Sidekiq.configure_server do |config|
   end
 end
 
+
+## Code Coverage
 require 'simplecov'
+
+SimpleCov.start do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb]
+end
 
 require File.expand_path("../../lib/sidekiq-rate-limiter", __FILE__)
 
